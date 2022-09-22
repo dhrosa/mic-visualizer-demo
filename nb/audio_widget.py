c9507de345c0429958fe1283f3f12dcec5726da9
Diony Rosa <dhrosa@gmail.com>
2022-09-22 14:29:04 -0400

    Split image update logic into ImageViewer.

--- nb/audio_widget.py
+++ nb/audio_widget.py
@@ -10,7 +10,7 @@ from weakref import WeakSet
 from matplotlib import colors, cm
 import matplotlib as mpl
 from si_prefix import si_format
-from threading import Thread
+from threading import Thread, Lock
 from collections import deque
 
 from audio_stream import IterThread, Broadcaster, AudioStream, serial_samples
@@ -20,7 +20,7 @@ class NewWindowDialog(QDialog):
         super().__init__()
         layout = QFormLayout(self)
         layout.addRow("Sample Rate", QLabel(str(fs)))
-        
+
         self.window_length = QComboBox()
         default_value = 1024
         default_index = 0
@@ -40,12 +40,11 @@ class NewWindowDialog(QDialog):
         buttons.rejected.connect(self.reject)
         layout.addRow(buttons)
 
-
 class ColormapDialog(QDialog):
     def __init__(self, parent, original_name):
         super().__init__(parent)
         self.icon_size = QSize(256, 32)
-        
+
         layout = QVBoxLayout(self)
         self.colormap_name = QComboBox()
         self.colormap_name.setIconSize(self.icon_size)
@@ -53,7 +52,7 @@ class ColormapDialog(QDialog):
             self.colormap_name.addItem(self.preview_icon(name), name)
         self.colormap_name.setCurrentText(original_name)
         layout.addWidget(self.colormap_name)
-        
+
         buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
@@ -66,12 +65,37 @@ class ColormapDialog(QDialog):
         colors = cmap(np.linspace(0, 1, self.icon_size.width()), bytes=True)
         for col, (r, g, b, a) in enumerate(colors):
             i = 4 * col
-            data[i:i+4] = bytes([b, g, r, a])            
+            data[i:i+4] = bytes([b, g, r, a])
         return QIcon(QPixmap.fromImage(image.scaled(self.icon_size)))
-                        
 
 
-class AudioWidget(QWidget):
+class ImageViewer(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.image_lock = Lock()
+        self.reset_image(1, 1)
+
+    def reset_image(self, width, height, fill_color=0xFF_00_00_00):
+        self.image = QImage(width, height, QImage.Format_ARGB32)
+        self.image.fill(fill_color)
+
+    def update_logical_rect(self, rect):
+        self.update(self.logical_to_device_rect(rect))
+
+    def logical_to_device_rect(self, rect):
+        transform = QTransform.fromScale(
+            self.width() / self.image.width(),
+            self.height() / self.image.height())
+        return transform.mapRect(rect).toAlignedRect()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        with self.image_lock:
+            painter.setWindow(self.image.rect())
+            painter.drawImage(0, 0, self.image)
+
+
+class AudioWidget(ImageViewer):
     def __init__(self, samples, fs, window_length):
         super().__init__()
         self.audio = AudioStream(samples, fs, window_length)
@@ -82,14 +106,15 @@ class AudioWidget(QWidget):
         self.set_colormap_name('viridis')
 
         self.column_index = 0
-        self.image = QImage(self.col_count, self.row_count, QImage.Format_RGB32)
-        self.image.fill(0xFF000000)
+        self.reset_image(self.col_count, self.row_count)
 
         self.init_shortcuts()
         self.update_thread = IterThread(self.process_audio())
 
+
     def init_shortcuts(self):
         QShortcut(QKeySequence("c"), self, self.colormap_prompt)
+
 
     def colormap_prompt(self):
         def on_complete(dialog):
@@ -98,11 +123,12 @@ class AudioWidget(QWidget):
         dialog.accepted.connect(lambda: on_complete(dialog))
         dialog.open()
 
+
     def process_audio(self):
         for psd in self.audio.psd_stream():
-            self.append_data(psd)
+            with self.image_lock:
+                self.append_data(psd)
             yield
-
 
     def sizeHint(self):
         return self.maximumSize()
@@ -111,12 +137,6 @@ class AudioWidget(QWidget):
     def closeEvent(self, event):
         self.update_thread.close()
         event.accept()
-
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setWindow(self.image.rect())
-        painter.drawImage(0, 0, self.image)
 
 
     @property
@@ -132,15 +152,9 @@ class AudioWidget(QWidget):
         self.column_index = 0
         data = self.data.copy()
         self.data.clear()
-        for psd in data:
-            self.append_data(psd)
-
-
-    def logical_to_device_rect(self, rect):
-        transform = QTransform.fromScale(
-            self.width() / self.image.width(),
-            self.height() / self.image.height())
-        return transform.mapRect(rect).toAlignedRect()
+        with self.image_lock:
+            for psd in data:
+                self.append_data(psd)
 
 
     def append_data(self, psd):
@@ -155,8 +169,7 @@ class AudioWidget(QWidget):
             i += stride
         self.column_index += 1
         self.column_index %= self.col_count
-        dirty_rect = self.logical_to_device_rect(QRectF(col, 0, 1, self.row_count))
-        self.update(dirty_rect)
+        self.update_logical_rect(QRectF(col, 0, 1, self.row_count))
 
 
 class Context(QObject):
