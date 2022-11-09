efd049adeb8713c0a5219a8b73bc401f9eaebf26
Diony Rosa <dhrosa@gmail.com>
2022-11-09 12:50:05 -0500

    Implement C++ LUT mapping.

--- nb/lut.cpp
+++ nb/lut.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 
+#include <algorithm>
 #include <cstdint>
 #include <stdexcept>
 #include <cstring>
@@ -13,17 +14,23 @@ struct Table {
   std::vector<uint32> entries;
 
   static Table FromRgbaArray(pybind11::array_t<uint8> arr);
+
+  void Map(double vmin,
+           double vmax,
+           pybind11::array_t<double> source,
+           pybind11::array_t<uint32> dest);
 };
 
-Table Table::FromRgbaArray(pybind11::array_t<uint8> arr) {
+
+Table FromRgbaArray(pybind11::array_t<uint8> arr) {
   if (arr.size() == 0) {
-    return Table();
+    throw std::invalid_argument("Input is empty..");
   }
   if (arr.ndim() != 2) {
-    throw std::runtime_error("Array must be 2D: " + std::to_string(arr.ndim()));
+    throw std::invalid_argument("Array must be 2D: " + std::to_string(arr.ndim()));
   }
   if (arr.shape(1) != 4) {
-    throw std::runtime_error("Innermost dimension must be 4: " + std::to_string(arr.shape(0)));
+    throw std::invalid_argument("Innermost dimension must be 4: " + std::to_string(arr.shape(0)));
   }
   const pybind11::ssize_t rows = arr.shape(0);
   const auto view = arr.unchecked<2>();
@@ -45,15 +52,55 @@ Table Table::FromRgbaArray(pybind11::array_t<uint8> arr) {
   return table;
 }
 
+void Table::Map(double vmin,
+                double vmax,
+                pybind11::array_t<double> source,
+                pybind11::array_t<uint32> dest) {
+  if (source.ndim() != 2) {
+    throw std::runtime_error("Source must be 2D: " + std::to_string(source.ndim()));
+  }
+  if (dest.ndim() != 2) {
+    throw std::runtime_error("Destination must be 2D: " + std::to_string(dest.ndim()));
+  }
+  for (int axis : {0, 1}) {
+    const int s = source.shape(axis);
+    const int d = dest.shape(axis);
+    if (s != d) {
+      throw std::runtime_error("Source and destination have mismatched shapes: " + std::to_string(s) + " vs " + std::to_string(d));
+    }
+  }
+  const double vrange = vmax - vmin;
+  const int height = source.shape(0);
+  const int width = source.shape(1);
+  const pybind11::ssize_t n = entries.size();
+  const auto source_view = source.unchecked<2>();
+  auto dest_view = dest.mutable_unchecked<2>();
+  const uint32* lut = entries.data();
+  for (int row = 0; row < height; ++row) {
+    for (int col = 0; col < width; ++col) {
+      const double clamped = std::clamp(source_view(row, col), vmin, vmax);
+      const double unbiased = clamped - vmin;
+      const double normalized = unbiased / vrange;
+      const pybind11::ssize_t index = normalized * (n - 1);
+      dest_view(row, col) = lut[index];
+    }
+  }
+}
 
 PYBIND11_MODULE(lut, m) {
   pybind11::class_<Table>(m, "Table")
-    .def(pybind11::init(&Table::FromRgbaArray))
-    .def_readonly("entries", &Table::entries);
+    .def(pybind11::init(&FromRgbaArray))
+    .def_readonly("entries", &Table::entries)
+    .def("Map", &Table::Map,
+         pybind11::arg("vmin"),
+         pybind11::arg("vmax"),
+         pybind11::arg("source").noconvert(),
+         pybind11::arg("dest").noconvert());
 }
 
 /*
 <%
 setup_pybind11(cfg)
+cfg['extra_compile_args'] = ['-std=c++17']
 %>
 */
--- nb/lut_test.py
+++ nb/lut_test.py
@@ -1,14 +1,16 @@
 import cppimport
 import unittest
+import numpy as np
+from numpy.testing import assert_equal as assert_array_equal
 
 lut = cppimport.imp_from_filepath("lut.cpp")
 
 from lut import Table
 
-class LutTest(unittest.TestCase):
+class LutEntryTest(unittest.TestCase):
     def test_empty(self):
-        table = Table([[]])
-        self.assertEqual(table.entries, [])
+        with self.assertRaisesRegex(ValueError, 'empty'):
+            table = Table([[]])
 
     def test_single(self):
         table = Table([[0x11, 0x22, 0x33, 0x44]])
@@ -21,6 +23,35 @@ class LutTest(unittest.TestCase):
         self.assertEqual([hex(e) for e in table.entries],
                          [hex(0x44112233),
                           hex(0x88556677)])
+
+
+class LutMapTest(unittest.TestCase):
+    def test_single(self):
+        table = Table([[0, 0, 1, 0]])
+        source = np.array([[10, 12.5],
+                           [15, 20]])
+        dest = np.zeros_like(source, dtype='uint32')
+        table.Map(10, 20, source, dest)
+
+        assert_array_equal([[1, 1],
+                            [1, 1]],
+                           dest)
+
+    def test_multi(self):
+        table = Table([[0, 0, 1, 0],
+                       [0, 0, 2, 0],
+                       [0, 0, 3, 0],
+                       [0, 0, 4, 0]])
+        # Overly small values should map to 1
+        # Overly large values should map to 4
+        source = np.array([[0.9, 1.0, 1.5],
+                           [1.75, 2.0, 2.1]])
+        dest = np.zeros_like(source, dtype='uint32')
+        table.Map(1, 2, source, dest)
+        assert_array_equal([[1, 1, 2],
+                            [3, 4, 4]],
+                           dest)
+
 
 if __name__ == '__main__':
     unittest.main()
