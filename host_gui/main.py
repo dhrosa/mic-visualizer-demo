e441f02c9f9a1c6752145b93c8be79907b506aa3
Diony Rosa <dhrosa@gmail.com>
2022-11-15 14:02:32 -0500

    Add py-spy and QThreadPool workaround

--- host_gui/main.py
+++ host_gui/main.py
@@ -2,10 +2,14 @@ import sys
 
 from context import Context
 
+from PySide6.QtCore import QThreadPool
 from PySide6.QtWidgets import QApplication
 
 
 def main():
+    # The default thread pool name has parentheses inside, which py-spy's thread
+    # name parsing doesn't handle correctly.
+    QThreadPool.globalInstance().setObjectName("Default QThreadPool")
     app = QApplication(sys.argv)
     Context(app)
     sys.exit(app.exec())
