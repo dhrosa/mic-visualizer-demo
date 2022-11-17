c03fe8427eeda746c82a4010e7bd40f1b02df01e
Diony Rosa <dhrosa@gmail.com>
2022-11-17 03:49:34 -0500

    Make Ctrl+C work.

--- host_gui/main.py
+++ host_gui/main.py
@@ -7,6 +7,13 @@ from context import Context
 from PySide6.QtCore import QThreadPool
 from PySide6.QtWidgets import QApplication
 
+# Qt's event loops prevents Python's default signal handler from being able to
+# execute. So instead we hookup the default signal handler which immediately
+# exits the process.
+import signal
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 
 def main():
     # The default thread pool name has parentheses inside, which py-spy's thread
