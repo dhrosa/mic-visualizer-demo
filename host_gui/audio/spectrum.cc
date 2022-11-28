91804067f2d9d6c1fd67ea614e986777ce03c944
Diony Rosa <dhrosa@gmail.com>
2022-11-28 14:38:25 -0500

    Initial spectrum implementation without tests.

--- host_gui/audio/spectrum.cc
+++ host_gui/audio/spectrum.cc
@@ -1,3 +1,25 @@
 #include "spectrum.h"
 
+#include <absl/cleanup/cleanup.h>
 #include <fftw3.h>
+
+#include <algorithm>
+#include <ranges>
+
+std::vector<std::complex<double>> Spectrum(
+    std::span<const std::int16_t> samples) {
+  const std::size_t n = samples.size();
+  std::vector<std::complex<double>> complex_samples(n);
+  std::transform(samples.begin(), samples.end(), complex_samples.begin(),
+                 [](auto x) { return static_cast<double>(x); });
+
+  std::vector<std::complex<double>> spectrum;
+  fftw_plan plan = fftw_plan_dft_1d(
+      n, reinterpret_cast<fftw_complex*>(complex_samples.data()),
+      reinterpret_cast<fftw_complex*>(spectrum.data()), FFTW_FORWARD,
+      FFTW_ESTIMATE);
+  auto plan_cleanup = [&]() { fftw_destroy_plan(plan); };
+
+  fftw_execute(plan);
+  return spectrum;
+}
--- host_gui/audio/spectrum.h
+++ host_gui/audio/spectrum.h
@@ -1,4 +1,9 @@
 #pragma once
 
+#include <complex>
 #include <cstdint>
+#include <span>
 #include <vector>
+
+std::vector<std::complex<double>> Spectrum(
+    std::span<const std::int16_t> samples);
