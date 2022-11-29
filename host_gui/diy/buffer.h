de38c374bd615e71ab817c11342008341640a689
Diony Rosa <dhrosa@gmail.com>
2022-11-29 16:10:53 -0500

    Initial Buffer implementation.

---
+++ host_gui/diy/buffer.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <absl/functional/any_invocable.h>
+
+#include <span>
+
+// Models a std::span with an RAII cleanup function that takes care of
+// cleaning the data backing the span.
+//
+// This allows for cheaply passing around contiguous data that can be
+// backed in versatile ways (e.g. by an std::vector, allocated with
+// special alignment, etc...).
+template <typename T>
+class Buffer {
+ public:
+  using value_type = std::span<T>::value_type;
+
+  Buffer() noexcept = default;
+  Buffer(std::span<T> span, absl::AnyInvocable<void() &&> cleanup) noexcept
+      : span_(span), cleanup_(std::move(cleanup)) {}
+  ~Buffer() {
+    if (cleanup_) {
+      std::move(cleanup_)();
+    }
+  }
+
+  Buffer(Buffer&&) noexcept = default;
+  Buffer& operator=(Buffer&&) noexcept = default;
+
+  std::span<T> span() const noexcept { return span_; }
+  auto begin() const noexcept { return span_.begin(); }
+  auto begin() noexcept { return span_.begin(); }
+  auto end() const noexcept { return span_.end(); }
+  auto end() noexcept { return span_.end(); }
+
+  std::span<T>::pointer data() const noexcept { return span_.data(); }
+  std::size_t size() const noexcept { return span_.size(); }
+  bool empty() const noexcept { return span_.empty(); }
+
+  std::span<T>::reference operator[](std::size_t i) const noexcept {
+    return span_[i];
+  }
+
+ private:
+  std::span<T> span_;
+  absl::AnyInvocable<void() &&> cleanup_;
+};
---
+++ host_gui/diy/buffer_test.cc
@@ -0,0 +1,68 @@
+#include "buffer.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <concepts>
+#include <ranges>
+#include <type_traits>
+
+using testing::ElementsAre;
+
+static_assert(std::is_nothrow_default_constructible_v<Buffer<int>>);
+static_assert(std::is_nothrow_default_constructible_v<Buffer<const int>>);
+
+static_assert(std::is_nothrow_move_constructible_v<Buffer<int>>);
+static_assert(std::is_nothrow_move_constructible_v<Buffer<const int>>);
+
+static_assert(!std::copy_constructible<Buffer<int>>);
+static_assert(!std::copy_constructible<Buffer<const int>>);
+
+static_assert(std::ranges::range<Buffer<int>>);
+static_assert(std::ranges::range<Buffer<const int>>);
+
+static_assert(std::ranges::contiguous_range<Buffer<int>>);
+static_assert(std::ranges::contiguous_range<Buffer<const int>>);
+
+static_assert(std::ranges::output_range<Buffer<int>, int>);
+static_assert(!std::ranges::output_range<Buffer<const int>, const int>);
+
+static_assert(std::ranges::sized_range<Buffer<int>>);
+static_assert(std::ranges::sized_range<Buffer<const int>>);
+
+static_assert(std::ranges::viewable_range<Buffer<int>>);
+static_assert(std::ranges::viewable_range<Buffer<const int>>);
+
+TEST(BufferTest, Empty) { EXPECT_TRUE(Buffer<int>().empty()); }
+
+TEST(BufferTest, NoOpCleanup) {
+  const int values[] = {1, 2, 3};
+  EXPECT_THAT(Buffer<const int>(values, [] {}), ElementsAre(1, 2, 3));
+}
+
+TEST(BufferTest, CleanupCalledOnce) {
+  bool cleanup_called = false;
+
+  const int values[] = {1, 2, 3};
+  {
+    auto buffer_a = Buffer<const int>(values, [&] { cleanup_called = true; });
+    EXPECT_FALSE(cleanup_called);
+    EXPECT_THAT(buffer_a, ElementsAre(1, 2, 3));
+
+    auto buffer_b = std::move(buffer_a);
+    EXPECT_FALSE(cleanup_called);
+    EXPECT_THAT(buffer_b, ElementsAre(1, 2, 3));
+  }
+  EXPECT_TRUE(cleanup_called);
+}
+
+TEST(BufferTest, Mutation) {
+  int values[] = {1, 2, 3};
+  auto buffer = Buffer<int>(values, [] {});
+  buffer[0] = 4;
+  buffer[1] = 5;
+  buffer[2] = 6;
+
+  EXPECT_THAT(buffer, ElementsAre(4, 5, 6));
+  EXPECT_THAT(values, ElementsAre(4, 5, 6));
+}
