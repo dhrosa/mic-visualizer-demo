3e6ce939020b5914c6cd4cb2a0d0a13db258aa01
Diony Rosa <dhrosa@gmail.com>
2022-12-02 14:23:09 -0500

    Complete 2D CircularBuffer implementation.

--- host_gui/image/circular_buffer.h
+++ host_gui/image/circular_buffer.h
@@ -1,23 +1,29 @@
 #pragma once
 
 #include <Eigen/Core>
+#include <ranges>
 #include <span>
 
+// Models a 2D image where new columns are appended to the right, causing older
+// columsn to be dropped from the left. To support this operation efficiently,
+// we don't expose the entire underlying 2D array to the caller, but instead
+// expose Older() and a Newer() 2D array accessors, which when concatenated
+// horizontally form the full image.
 template <typename T>
 class CircularBuffer {
  public:
   CircularBuffer(std::size_t width, std::size_t height, T fill = T{})
-      : data_(height, width) {
+      : data_(height, width), next_column_(width) {
     data_.fill(fill);
   }
 
-  auto Newer() const noexcept { return data_.leftCols(current_column_); }
+  auto Newer() const noexcept { return data_.leftCols(next_column_); }
 
   auto Older() const noexcept {
-    return data_.rightCols(columns() - current_column_);
+    return data_.rightCols(columns() - next_column_);
   }
 
-  void AppendColumn(std::span<const T> new_column);
+  void AppendColumn(std::span<const T> column) noexcept;
 
   std::size_t columns() const noexcept { return data_.cols(); }
   std::size_t rows() const noexcept { return data_.rows(); }
@@ -27,8 +33,12 @@ class CircularBuffer {
 
  private:
   Eigen::Array<T, Eigen::Dynamic, Eigen::Dynamic> data_;
-  Eigen::Index current_column_ = 0;
+  Eigen::Index next_column_ = 0;
 };
 
 template <typename T>
-void CircularBuffer<T>::AppendColumn(std::span<const T> new_column) {}
+void CircularBuffer<T>::AppendColumn(std::span<const T> column) noexcept {
+  next_column_ %= columns();
+  std::ranges::copy(column, data_.col(next_column_).begin());
+  ++next_column_;
+}
--- host_gui/image/circular_buffer_test.cc
+++ host_gui/image/circular_buffer_test.cc
@@ -3,7 +3,42 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+using testing::AllOf;
 using testing::ElementsAre;
+using testing::ElementsAreArray;
+using testing::Matcher;
+using testing::ResultOf;
+
+// gMock matchers against Eigen array.
+
+auto HasWidth(auto width) {
+  return ResultOf(
+      "width", [](auto array) { return array.cols(); }, width);
+}
+
+auto HasHeight(auto height) {
+  return ResultOf(
+      "height", [](auto array) { return array.rows(); }, height);
+}
+
+auto HasDimensions(auto width, auto height) {
+  return AllOf(HasWidth(width), HasHeight(height));
+}
+
+auto ArrayElementsAre(std::vector<std::vector<int>> expected) {
+  auto as_2d_vector = [](const auto& array) {
+    std::vector<std::vector<int>> values;
+    for (const auto& row : array.rowwise()) {
+      values.emplace_back(row.begin(), row.end());
+    }
+    return values;
+  };
+  std::vector<Matcher<std::vector<int>>> row_matchers;
+  for (const auto& row : expected) {
+    row_matchers.push_back(ElementsAreArray(row));
+  }
+  return ResultOf("data", as_2d_vector, ElementsAreArray(row_matchers));
+}
 
 TEST(CircularBufferTest, Dimensions) {
   CircularBuffer<int> buffer(4, 5);
@@ -13,13 +48,35 @@ TEST(CircularBufferTest, Dimensions) {
   EXPECT_EQ(buffer.rows(), 5);
 }
 
-TEST(CircularBufferTest, EmptyBufferOlderBiased) {
+TEST(CircularBufferTest, EmptyBuffer) {
   CircularBuffer<int> buffer(2, 2);
-  EXPECT_EQ(buffer.Newer().cols(), 0);
-  EXPECT_EQ(buffer.Older().cols(), 2);
+  EXPECT_THAT(buffer.Newer(), ArrayElementsAre({{0, 0}, {0, 0}}));
+  EXPECT_THAT(buffer.Older(), ArrayElementsAre({{}, {}}));
 }
 
 TEST(CircularBufferTest, Fill) {
   CircularBuffer<int> buffer(2, 2, 86);
-  EXPECT_THAT(buffer.Older().reshaped(), ElementsAre(86, 86, 86, 86));
+  EXPECT_THAT(buffer.Newer(), ArrayElementsAre({{86, 86}, {86, 86}}));
+  EXPECT_THAT(buffer.Older(), ArrayElementsAre({{}, {}}));
+}
+
+TEST(CircularBufferTest, Append) {
+  CircularBuffer<int> buffer(3, 2);
+
+  buffer.AppendColumn(std::span<const int>({1, 2}));
+  EXPECT_THAT(buffer.Older(), ArrayElementsAre({{0, 0}, {0, 0}}));
+  EXPECT_THAT(buffer.Newer(), ArrayElementsAre({{1}, {2}}));
+
+  buffer.AppendColumn(std::span<const int>({3, 4}));
+  EXPECT_THAT(buffer.Older(), ArrayElementsAre({{0}, {0}}));
+  EXPECT_THAT(buffer.Newer(), ArrayElementsAre({{1, 3}, {2, 4}}));
+
+  buffer.AppendColumn(std::span<const int>({5, 6}));
+  EXPECT_THAT(buffer.Older(), ArrayElementsAre({{}, {}}));
+  EXPECT_THAT(buffer.Newer(), ArrayElementsAre({{1, 3, 5}, {2, 4, 6}}));
+
+  // Full loop around; the {1, 2} column should be dropped.
+  buffer.AppendColumn(std::span<const int>({7, 8}));
+  EXPECT_THAT(buffer.Older(), ArrayElementsAre({{3, 5}, {4, 6}}));
+  EXPECT_THAT(buffer.Newer(), ArrayElementsAre({{7}, {8}}));
 }
